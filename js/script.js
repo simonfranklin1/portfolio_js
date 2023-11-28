cd160d257a9d0e9bc43cbe67e6e8a8556e9a7e90
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2023-11-27 21:37:17 -0300

    Continuando lógica da página e iniciando scroll reveal

--- js/script.js
+++ js/script.js
@@ -12,3 +12,95 @@ skillIcons.forEach((skill, index) => {
         skillDescriptions[0].classList.remove("hidden");
     });
 })
+
+window.show = ScrollReveal({reset: true});
+
+/* Home */
+
+show.reveal('.home-header', {
+    duration: 2000
+}) 
+
+show.reveal('#home button', {
+    duration: 2000,
+    distance: '50%',
+    delay: 500
+})
+
+show.reveal('.title', {
+    duration: 2000,
+    distance: '90px'
+})
+
+/* About */
+
+show.reveal('.about-image', {
+    duration: 2000,
+    origin: 'left',
+    distance: '90px',
+    delay: 500
+})
+
+show.reveal('.informations', {
+    duration: 2000,
+    origin: 'right',
+    distance: '90px',
+    delay: 800
+})
+
+show.reveal('.timeline', {
+    duration: 2000,
+    delay: 1000
+})
+
+
+/* Portfolio */
+
+show.reveal('.project-1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+show.reveal('.project-2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+show.reveal('.project-3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+/* Skills */
+
+show.reveal('.box-1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+show.reveal('.box-2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 800
+})
+
+show.reveal('.box-3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1100
+})
+
+show.reveal('.box-4', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1300
+})
+
+show.reveal('.skills-description', {
+    duration: 2000,
+    delay: 500
+})
